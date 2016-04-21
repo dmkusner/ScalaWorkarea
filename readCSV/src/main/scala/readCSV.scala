dbcc23efc4a629600f0d5cba55295f51ab0309db
David Kusner <dmkusner@gmail.com>
2016-04-21 18:08:05 -0400

    close file while still using iterator

--- readCSV/src/main/scala/readCSV.scala
+++ readCSV/src/main/scala/readCSV.scala
@@ -1,19 +1,16 @@
 
 object readCSV {
 
-    def printCSV(csvfile: String): Unit = {
-        val bufferedSource = io.Source.fromFile(csvfile)
-	for (line <- bufferedSource.getLines) println(line)
-	bufferedSource.close
-    }
-
     def parseCSV(csvfile: String): Iterator[Map[String,String]] = {
         val bufferedSource = io.Source.fromFile(csvfile)
-	val lines = (bufferedSource.getLines).toArray
-	bufferedSource.close
-	val iterLines = lines.toIterator
-        val header = iterLines.next.split(",").map(_.trim)
-	for (line <- iterLines) yield (header zip line.split(",").map(_.trim)).toMap
+	val lines = bufferedSource.getLines
+        val header = lines.next.split(",").map(_.trim)
+
+        for (line <- lines if lines.hasNext) yield {
+	    if (lines.isEmpty) { bufferedSource.close }
+	    val dataMap = (header zip line.split(",").map(_.trim)).toMap
+	    dataMap
+	}
     }
 
     def main(args: Array[String]): Unit = {
